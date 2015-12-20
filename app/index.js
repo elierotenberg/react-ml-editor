0b257b5e1d7ce8de1d94195fe35a7881643d910a
Elie Rotenberg <elie@rotenberg.io>
2015-12-20 17:25:12 +0100

    Refining for release
    Demo has been moved out of this repo
    
    Components are now correctly exported

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
 import 'babel-polyfill';
+
+import ReactMLContextMenu from './ReactMLContextMenu';
+import ReactMLEditor from './ReactMLEditor';
+import ReactMLHelpMenu from './ReactMLHelpMenu';
+import ReactMLPreview from './ReactMLPreview';
+import ReactMLSplitEditor from './ReactMLSplitEditor';
+import ReactMLSuggestMenu from './ReactMLSuggestMenu';
+
+export default {
+  ReactMLContextMenu,
+  ReactMLEditor,
+  ReactMLHelpMenu,
+  ReactMLPreview,
+  ReactMLSplitEditor,
+  ReactMLSuggestMenu,
+};
